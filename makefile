ea441b33813208e346d07d6e73f2f167b6cb3137
Liam Bindle <lrb175@mail.usask.ca>
2018-04-05 16:21:34 -0600

    added some simple example

--- makefile
+++ makefile
@@ -14,7 +14,7 @@ OBJECTS := $(addprefix $(OBJDIR)/,$(SOURCES:%.c=%.o))
 LIBMQTT_TARGET := $(BINDIR)/libmqtt.so
 LIBMQTT_DEPENDENCIES := $(OBJECTS)
 
-all: dirs $(LIBMQTT_TARGET) tests
+all: dirs $(LIBMQTT_TARGET) tests simple_publisher simple_subscriber
 
 $(OBJDIR)/%.o: $(SRCDIR)/%.c
 	$(CC) -c $(CFLAGS) $< -o $@
@@ -30,5 +30,11 @@ TESTS_CFLAGS := -Wno-unused-parameter -Wunused-variable -Wl,-rpath=$(abspath ./b
 tests: tests.c $(LIBMQTT_TARGET)
 	$(CC) $(CFLAGS) $(TESTS_CFLAGS) $< -lcmocka -lmqtt -o $@
 
+simple_publisher: examples/simple_publisher.c src/mqtt.c src/mqtt_pal.c 
+	$(CC) $(CFLAGS) -Wno-unused-parameter -Wunused-variable $^ -lpthread -o $@
+
+simple_subscriber: examples/simple_subscriber.c src/mqtt.c src/mqtt_pal.c 
+	$(CC) $(CFLAGS) -Wno-unused-parameter -Wunused-variable $^ -lpthread -o $@
+
 clean:
-	rm -rf obj bin tests
+	rm -rf obj bin tests simple_publisher simple_subscriber
--- src/mqtt_pal.c
+++ src/mqtt_pal.c
@@ -8,7 +8,13 @@
 
 #ifdef __unix__
 
+#include <stdio.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <fcntl.h>
+
 
 ssize_t mqtt_pal_sendall(int fd, const void* buf, size_t len, int flags) {
     size_t sent = 0;
@@ -38,6 +44,44 @@ ssize_t mqtt_pal_recvall(int fd, void* buf, size_t bufsz, int flags) {
     } while (rv > 0);
 
     return buf - start;
+}
+
+
+int mqtt_pal_sockopen(const char* addr, const char* port, int af) {
+    struct addrinfo hints = {0};
+
+    hints.ai_family = af; /* IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Must be TCP */
+    int sockfd = -1;
+    int rv;
+    struct addrinfo *p, *servinfo;
+
+    /* get address information */
+    rv = getaddrinfo(addr, port, &hints, &servinfo);
+    if(rv != 0) {
+        fprintf(stderr, "Failed to open socket (getaddrinfo): %s\n", gai_strerror(rv));
+        return -1;
+    }
+
+    /* open the first possible socket */
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sockfd == -1) continue;
+
+        /* connect to server */
+        rv = connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
+        if(rv == -1) continue;
+        break;
+    }  
+
+    /* free servinfo */
+    freeaddrinfo(servinfo);
+
+    /* make non-blocking */
+    if (sockfd != -1) fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL) | O_NONBLOCK);
+
+    /* return the new socket fd */
+    return sockfd;  
 }
 
 #endif
