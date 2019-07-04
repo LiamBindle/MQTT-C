4458ec2a225ac50695de333f95e58ae19f6ca408
Carlos Mancón <manconci@gmail.com>
2019-07-03 22:02:28 -0300

    Fixed 'DUP' Flag mislocation for re-delivering QoS1 publish packets
    When using QoS 1, in case of re-delivering a packet for which a PUBACK was not received, the DUP Flag (Byte 1, bit 3 of Fixed Header) must be set to 1. This was considered but the flag was being set on Byte 2, modifying the Remaining Length field.

--- src/mqtt.c
+++ src/mqtt.c
@@ -561,7 +561,7 @@ ssize_t __mqtt_send(struct mqtt_client *client)
             } else if (inspected == 1) {
                 msg->state = MQTT_QUEUED_AWAITING_ACK;
                 /*set DUP flag for subsequent sends */ 
-                msg->start[1] |= MQTT_PUBLISH_DUP;
+                msg->start[0] |= MQTT_PUBLISH_DUP;
             } else {
                 msg->state = MQTT_QUEUED_AWAITING_ACK;
             }
