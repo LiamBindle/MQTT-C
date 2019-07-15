706e6870c5b5150b4ff27765198d3f2ac435a7d8
Carlos Mancón <manconci@gmail.com>
2019-07-14 21:40:49 -0300

    Fixed inspection of QoS flags in new code
    When the use of the QOS mask was implemented, a bug was introduced. This commit fix this by masking and then shifting.

--- src/mqtt.c
+++ src/mqtt.c
@@ -555,7 +555,7 @@ ssize_t __mqtt_send(struct mqtt_client *client)
             msg->state = MQTT_QUEUED_COMPLETE;
             break;
         case MQTT_CONTROL_PUBLISH:
-            inspected = MQTT_PUBLISH_QOS_MASK & ((msg->start[0]) >> 1); /* qos */
+            inspected = ( MQTT_PUBLISH_QOS_MASK & (msg->start[0]) ) >> 1; /* qos */
             if (inspected == 0) {
                 msg->state = MQTT_QUEUED_COMPLETE;
             } else if (inspected == 1) {
