33341d55b6f8c5df8306ef23b8fdbfbd099294e9
Mark Radbourne <markrad@msn.com>
2020-06-29 15:40:47 -0700

    Add support for BearSSL

--- src/mqtt_pal.c
+++ src/mqtt_pal.c
@@ -86,6 +86,125 @@ ssize_t mqtt_pal_recvall(mqtt_pal_socket_handle fd, void* buf, size_t bufsz, int
     return buf - start;
 }
 
+#elif defined(MQTT_USE_BEARSSL)
+#include <bearssl.h>
+#include <memory.h>
+
+static int do_rec_data(mqtt_pal_socket_handle fd, unsigned int status) {
+    ssize_t rc;
+    uint8_t *buffer;
+    size_t length;
+    int err;
+
+    err = br_ssl_engine_last_error(&fd->sc.eng);
+
+    if (err != BR_ERR_OK) {
+        return MQTT_ERROR_SOCKET_ERROR;
+    }
+
+    if ((status & BR_SSL_SENDREC) == BR_SSL_SENDREC) {
+        buffer = br_ssl_engine_sendrec_buf(&fd->sc.eng, &length);
+
+        if (length > 0) {
+            rc = fd->low_write(&fd->fd, buffer, length);
+            br_ssl_engine_sendrec_ack(&fd->sc.eng, rc);
+        }
+    }
+    else if ((status & BR_SSL_RECVREC) == BR_SSL_RECVREC) {
+        buffer = br_ssl_engine_recvrec_buf(&fd->sc.eng, &length);
+
+        if (length > 0) {
+            rc = fd->low_read(&fd->fd, buffer, length);
+            br_ssl_engine_recvrec_ack(&fd->sc.eng, rc);
+        }
+    }
+    else if ((status && BR_SSL_CLOSED) == BR_SSL_CLOSED) {
+        return MQTT_ERROR_SOCKET_ERROR;
+    }
+
+    return MQTT_OK;
+}
+
+ssize_t mqtt_pal_sendall(mqtt_pal_socket_handle fd, const void* buf, size_t len, int flags) {
+    int rc;
+    uint8_t *buffer;
+    size_t length;
+    size_t remaining_bytes = len;
+    const uint8_t *walker = buf;
+    unsigned int status;
+
+    while (remaining_bytes > 0) {
+
+        if (rc == MQTT_ERROR_SOCKET_ERROR) {
+            return rc;
+        }
+
+        status = br_ssl_engine_current_state(&fd->sc.eng);
+
+        if ((status & (BR_SSL_RECVREC | BR_SSL_SENDREC)) != 0) {
+            rc = do_rec_data(fd, status);
+
+            if (rc != MQTT_OK) {
+                return rc;
+            }
+            status = br_ssl_engine_current_state(&fd->sc.eng);
+        }
+
+        if ((status & BR_SSL_SENDAPP) == BR_SSL_SENDAPP) {
+            buffer = br_ssl_engine_sendapp_buf(&fd->sc.eng, &length);
+
+            if (length > 0) {
+                size_t write = length >= remaining_bytes? remaining_bytes : length;
+                memcpy(buffer, walker, write);
+                remaining_bytes -= write;
+                walker += write;
+                br_ssl_engine_sendapp_ack(&fd->sc.eng, write);
+                br_ssl_engine_flush(&fd->sc.eng, 0);
+            }
+        }
+    }
+
+    return len;
+}
+
+ssize_t mqtt_pal_recvall(mqtt_pal_socket_handle fd, void* buf, size_t bufsz, int flags) {
+    int rc;
+    uint8_t *buffer;
+    size_t length;
+    size_t remaining_bytes = bufsz;
+    uint8_t *walker = buf;
+    unsigned int status;
+
+    if (rc == MQTT_ERROR_SOCKET_ERROR) {
+        return rc;
+    }
+
+    status = br_ssl_engine_current_state(&fd->sc.eng);
+
+    if ((status & (BR_SSL_RECVREC | BR_SSL_SENDREC)) != 0) {
+        rc = do_rec_data(fd, status);
+
+        if (rc != MQTT_OK) {
+            return rc;
+        }
+        status = br_ssl_engine_current_state(&fd->sc.eng);
+    }
+
+    if ((status & BR_SSL_RECVAPP) == BR_SSL_RECVAPP) {
+        buffer = br_ssl_engine_recvapp_buf(&fd->sc.eng, &length);
+
+        if (length > 0) {
+            size_t write = length >= remaining_bytes? remaining_bytes : length;
+            memcpy(walker, buffer, write);
+            remaining_bytes -= write;
+            walker += write;
+            br_ssl_engine_recvapp_ack(&fd->sc.eng, write);
+        }
+    }
+
+    return bufsz - remaining_bytes;
+}
+
 #elif defined(MQTT_USE_BIO)
 #include <openssl/bio.h>
 #include <openssl/ssl.h>
