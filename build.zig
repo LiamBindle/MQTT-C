3d983ab3b351b2ed4a49a62409b39b37afd6c5f9
Abdulkerim Güven <akgvn@users.noreply.github.com>
2022-06-16 20:35:35 +0300

    Enable cross-compilation for Linux x86_64 and ARM64

--- build.zig
+++ build.zig
@@ -1,12 +1,9 @@
 // Used the following as cross-compilation build.zig example:
 // https://git.sr.ht/~jamii/focus/tree/master/build.zig
 
-const builtin = @import("builtin");
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
-    // var target = b.standardTargetOptions(.{});
-
     const windows = b.addStaticLibrary("mqtt-c", null);
     try includeCommon(windows);
     windows.setTarget(std.zig.CrossTarget{
@@ -15,30 +12,52 @@ pub fn build(b: *std.build.Builder) !void {
     });
     windows.install();
 
-    const m1_mac = b.addStaticLibrary("mqtt-c-m1", null);
-    try includeCommon(m1_mac);
-    m1_mac.setTarget(std.zig.CrossTarget{
+    const apple_silicon_mac = b.addStaticLibrary("mqtt-c-apple-silicon", null);
+    try includeCommon(apple_silicon_mac);
+    apple_silicon_mac.setTarget(std.zig.CrossTarget{
         .cpu_arch = .aarch64,
         .os_tag = .macos,
     });
-    m1_mac.install();
+    apple_silicon_mac.install();
 
-    const x64_mac = b.addStaticLibrary("mqtt-c-x64", null);
-    try includeCommon(x64_mac);
-    x64_mac.setTarget(std.zig.CrossTarget{
+    const x86_64_mac = b.addStaticLibrary("mqtt-c-mac-x64", null);
+    try includeCommon(x86_64_mac);
+    x86_64_mac.setTarget(std.zig.CrossTarget{
         .cpu_arch = .x86_64,
         .os_tag = .macos,
     });
-    x64_mac.install();
+    x86_64_mac.install();
+
+    const x86_64_linux = b.addStaticLibrary("mqtt-c-x64", null);
+    try includeCommon(x86_64_linux);
+    x86_64_linux.setTarget(std.zig.CrossTarget{
+        .cpu_arch = .x86_64,
+        .os_tag = .linux,
+    });
+    x86_64_linux.install();
+
+    const arm64_linux = b.addStaticLibrary("mqtt-c-arm64", null);
+    try includeCommon(arm64_linux);
+    arm64_linux.setTarget(std.zig.CrossTarget{
+        .cpu_arch = .aarch64,
+        .os_tag = .linux,
+    });
+    arm64_linux.install();
 
     const windows_step = b.step("windows", "Build for Windows");
     windows_step.dependOn(&windows.step);
 
-    const m1_mac_step = b.step("m1_mac", "Build for M1 Mac");
-    m1_mac_step.dependOn(&m1_mac.step);
+    const apple_silicon_mac_step = b.step("apple_silicon_mac", "Build for Apple Silicon Macs");
+    apple_silicon_mac_step.dependOn(&apple_silicon_mac.step);
 
-    const x64_mac_step = b.step("x64_mac", "Build for M1 Mac");
-    x64_mac_step.dependOn(&x64_mac.step);
+    const x86_64_mac_step = b.step("x86_64_mac", "Build for Intel Macs");
+    x86_64_mac_step.dependOn(&x86_64_mac.step);
+
+    const x86_64_linux_step = b.step("x86_64_linux", "Build for Linux");
+    x86_64_linux_step.dependOn(&x86_64_linux.step);
+
+    const arm64_linux_step = b.step("arm64_linux", "Build for ARM64 Linux");
+    arm64_linux_step.dependOn(&arm64_linux.step);
 }
 
 fn includeCommon(lib: *std.build.LibExeObjStep) !void {
